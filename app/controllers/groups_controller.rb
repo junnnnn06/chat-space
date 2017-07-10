0418c4d1ffed3500383d28ba735bc43450e36a89
junnnnn06 <JUN@adachijun-no-MacBook-Air-2.local>
2017-07-10 13:25:12 +0900

    edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,7 @@
 class GroupsController < ApplicationController
 
   def index
-    @message = Message.new
+
   end
 
   def new
--- app/models/group.rb
+++ app/models/group.rb
@@ -2,10 +2,4 @@ class Group < ApplicationRecord
   has_many :members
   has_many :users, through: :members
   validates :name, presence: true
-
-  def add_error_message
-    if name.empty?
-      errors.add(:name, "が記載されていません")
-    end
-  end
 end
