2a0c8e9756895978d980fceb1096e664cee19b9b
junnnnn06 <JUN@adachijun-no-MacBook-Air-2.local>
2017-07-11 18:56:13 +0900

    createアクションの設定、不要ファイルの削除、

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,17 @@ class MessagesController < ApplicationController
   end
 
   def create
+    @message = Message.new(message_params)
+    if message.save
+      redirect_to group_message_path, notice: "メッセージ送信成功"
+    else
+      flash.now[:alert] = "メッセージを入力して下さい"
+      render :index
+    end
   end
 
+  private
+  def message_params
+    params.require(:message).permit(:image, :body).merge(user_id: current_user.id)
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   validates :user_id, null: false, foreign_key: true
   validates :group_id, null: false, foreign_key: true
+  validates :name, :image, null: false
 
   belongs_to :user
   belongs_to :member
