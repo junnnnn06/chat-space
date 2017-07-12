5fa377a40a2c2d44ffd279d1d45283c81e092b02
junnnnn06 <JUN@adachijun-no-MacBook-Air-2.local>
2017-07-12 20:18:01 +0900

    .gitignoreの設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,18 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!, only: :index
+  # before_action :set_group, only: [:index, :create]
+  # before_action :set_group_info, only: [:index, :create]
+
   def index
     @groups = current_user.groups
+    @message = Message.new
   end
 
   def create
+    @groups = current_user.groups
     @message = Message.new(message_params)
-    if message.save
-      redirect_to group_message_path, notice: "メッセージ送信成功"
+    if @message.save
+      redirect_to action: 'index', notice: "メッセージ送信成功"
     else
       flash.now[:alert] = "メッセージを入力して下さい"
       render :index
@@ -16,6 +21,14 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:message).permit(:image, :body).merge(user_id: current_user.id)
+    params.require(:message).permit(:image, :body).merge(user_id: current_user.id, group_id: params[:group_id])
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+  def set_group_info
+    self.group_id = current_user.group_id
   end
 end
