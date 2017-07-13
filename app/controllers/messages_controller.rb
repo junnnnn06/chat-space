f5e245024d0ac9e95378f66ed0fdaba51db9dd40
junnnnn06 <JUN@adachijun-no-MacBook-Air-2.local>
2017-07-13 16:43:30 +0900

    投稿したメッセージをビューに表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,14 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!, only: :index
-  before_action :set_group, only: [:index, :create]
+  before_action :set_groups, :set_group_info, only: [:index, :create]
 
   def index
-    @groups
+    @messages = @group.messages
     @message = Message.new
   end
 
   def create
-    @groups
+    @messages = @group.messages
     @message = Message.new(message_params)
     if @message.save
       redirect_to group_messages_path(params[:group_id]), notice: "メッセージ送信成功"
@@ -23,7 +23,11 @@ class MessagesController < ApplicationController
     params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:group_id])
   end
 
-  def set_group
+  def set_groups
     @groups = current_user.groups
+  end
+
+  def set_group_info
+    @group = Group.find(params[:group_id])
   end
 end
