e14d946ea5bce342bbfa45a2312bdc40f409f9be
junnnnn06 <JUN@adachijun-no-MacBook-Air-2.local>
2017-07-18 16:17:30 +0900

    jsファイル生成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -17,7 +17,7 @@ class MessagesController < ApplicationController
       render :index
     end
   end
-  
+
   private
   def message_params
     params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:group_id])
