cec98a5588a2c2118a4298c9f5b3282f736b68b2
junnnnn06 <JUN@adachijun-no-MacBook-Air-2.local>
2017-07-19 18:18:31 +0900

    ルーティング設定、viewファイル作成、イベント発火

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,6 @@
+$(function() {
+  $(".chat-group-form__input").on("keyup", function() {
+    var input = $("#user-search-field").val();
+    console.log(input);
+  });
+});
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.search_by_name(params[:keyword])
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def edit
   end
 
