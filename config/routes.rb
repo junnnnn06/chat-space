e069e3f957870cc1b6bdf8b5ea28b0a2bd1c14d7
junnnnn06 <JUN@adachijun-no-MacBook-Air-2.local>
2017-07-20 13:53:02 +0900

    編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resources :users, except: [:index, :edit, :new, :update]
-  resources :groups, only: [:new, :create, :edit, :index, :update] do
+  resources :users, except: [:create, :show, :destroy]
+  resources :groups, except: [:destroy, :show] do
     resources :messages, only: [:index, :create]
   end
 end
