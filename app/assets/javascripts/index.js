c1b6bbf9875d2875e0d6d4d0fdbddbfde4ab2b2f
junnnnn06 <JUN@adachijun-no-MacBook-Air-2.local>
2017-07-20 12:45:47 +0900

    console.log 削除

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -33,7 +33,6 @@ $(function() {
       $("#user-search-result").empty();
       if (users.length !==0) {
         users.forEach(function(user) {
-          console.log(user);
           appendUser(user);
         });
       }
